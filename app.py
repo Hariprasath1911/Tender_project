8c23355c3e29b45079de5a7cd24f337733c19920
Hariprasath1911 <hari07011996@gmail.com>
2025-04-28 18:31:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 import streamlit as st
 import json
 from matcher.ai_matcher import match_tenders
+import base64
+st.set_page_config(page_title="Amazon Prime Chatbot", page_icon="🎬")
+def set_background(image_file):
+    with open(image_file, "rb") as image:
+        encoded = base64.b64encode(image.read()).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
+set_background(r"pg.jpg")
 def load_tenders(filepath):
     with open(filepath, 'r') as f:
         return json.load(f)
